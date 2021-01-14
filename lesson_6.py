2fcc0d5eecebe93f58c8a0c7e88004246a9aadf3
@shol1zar <werlok221b@gmail.com>
2021-01-14 16:17:48 +0600

    home work

---
+++ lesson_6.py
@@ -0,0 +1,17 @@
+# from mymath import other
+import mymath as m
+# import mymath.operations.trigonometric as trig
+# import mymath.operations.arithmetic as arith
+
+# print(other.rounds())
+
+# m.operations.arithmetic.add()
+
+
+# # Sinus
+# intr = int(input("Smth: "))
+# intr2 = int(input("Smth: "))
+# print(trig.sine(intr, intr2))
+#
+# # Add
+# print(arith.add(1, 2))
---
+++ practice.py
@@ -0,0 +1,58 @@
+board = [[], [], []]
+
+
+def printo():
+    print(board[0])
+    print(board[1])
+    print(board[2])
+
+
+i = 1
+while i < 10:
+    add = input("Первым ходит Х потом 0 ведите индекс, например(11): ")
+    value = ""
+    if i % 2 == 0:
+        value = '0'
+    else:
+        value = 'х'
+    i += 1
+
+    if add == '00':
+        board[0].insert(0, value)
+        printo()
+    elif add == '01':
+        board[0].insert(1, value)
+        printo()
+    elif add == '02':
+        board[0].insert(2, value)
+        printo()
+
+    elif add == '10':
+        board[1].insert(0, value)
+        printo()
+    elif add == '11':
+        board[1].insert(1, value)
+        printo()
+    elif add == '12':
+        board[1].insert(2, value)
+        printo()
+
+    elif add == '20':
+        board[2].insert(0, value)
+        printo()
+    elif add == '21':
+        board[2].insert(1, value)
+        printo()
+    elif add == '22':
+        board[2].insert(2, value)
+        printo()
+
+
+file = 'result.txt'
+s = ""
+for n in board:
+    s += str(n) + '\n'
+
+f = open(file, 'a')
+f.write("\n" + s)
+f.close()

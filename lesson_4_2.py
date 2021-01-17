f8fd01f4de5a32aab1d6758ce7f87253b882b568
@shol1zar <werlok221b@gmail.com>
2021-01-17 18:22:43 +0600

    fix function

--- lesson_4_2.py
+++ lesson_4_2.py
@@ -1,15 +1,20 @@
 data = list()
 
 
-def my_func(**kwargs):
-    data.append(kwargs)
+def my_func(a):
+    return a
 
 
 while len(data) < 3:
-    my_func(
-            car=input("Введите марку машины: "),
-            model=input("Введите модель: "),
-            year=input("Введите год выпуска: ")
-            )
+    car = input("Введите марку машины: ")
+    model = input("Введите модель: ")
+    year = input("Введите год выпуска: ")
+
+    diction = {
+        "car": car,
+        "model": model,
+        "year": year
+                }
+    data.append(my_func(diction))
 
 print(data)
